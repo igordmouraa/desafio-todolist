d20c1e41f475d4df451fa68f24c8396c2f823154
igordmouraa <igordmoura_@hotmail.com>
2024-11-14 15:01:55 -0300

    feat(entity): create Todo entity with basic fields and JPA annotations

---
+++ src/main/java/br/com/igormoura/desafio_todolist/entity/Todo.java
@@ -0,0 +1,28 @@
+package br.com.igormoura.desafio_todolist.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "todos")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private boolean fulfilled;
+}
