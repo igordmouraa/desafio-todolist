12bbb6c10d423896d4ed71dcc6f0374491d720ad
igordmouraa <igordmoura_@hotmail.com>
2024-11-14 13:38:22 -0300

    feat: Initial commit

---
+++ src/test/java/br/com/igormoura/desafio_todolist/DesafioTodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.igormoura.desafio_todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioTodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
